ecc5dbe5d6da5e56bd6437fa50f37a7f7a3fb672
vv-ar456 <gamerbhai552068@gmail.com>
2026-03-12 11:53:14 +0530

    Refactor order placement function with Supabase

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-await fetch(SUPABASE_URL+"/rest/v1/orders",{
+const supabase = window.supabase.createClient(
+"https://blrsnfmljaycubcysbfw.supabase.co",
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscnNuZm1samF5Y3ViY3lzYmZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyNjcxMDcsImV4cCI6MjA4ODg0MzEwN30.d_oh9H-GMJ5M0wiEDonuVrJQSKqCGPToeuaIDNohIxs"
+)
 
-method:"POST",
+function openForm(){
+document.getElementById("orderForm").style.display="block"
+window.scrollTo(0,document.body.scrollHeight)
+}
 
-headers:{
+async function placeCOD(){
 
-"Content-Type":"application/json",
+let name=document.getElementById("name").value
+let phone=document.getElementById("phone").value
+let address=document.getElementById("address").value
+let pincode=document.getElementById("pincode").value
 
-"apikey":SUPABASE_KEY,
+if(!name || !phone || !address){
+alert("Please fill details")
+return
+}
 
-"Authorization":"Bearer "+SUPABASE_KEY,
-
-"Prefer":"return=minimal"
-
-},
-
-body:JSON.stringify({
-
+const {data,error}=await supabase
+.from("orders")
+.insert([
+{
 name:name,
-
 phone:phone,
-
 address:address,
-
 pincode:pincode,
+payment:"COD"
+}
+])
 
-payment_method:"COD"
+if(error){
+alert("Order failed")
+}else{
+alert("Order placed successfully")
+}
 
-})
-
-})
+}
